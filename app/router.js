813a8872f43218dae5852ff1220daf2eb074ebd2
Tao Ning <tning@linkedin.com>
2018-06-16 10:29:36 -0400

    remove mobile disabled

--- app/router.js
+++ app/router.js
@@ -1,16 +1,15 @@
-import EmberRouter from "@ember/routing/router";
-import config from "./config/environment";
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function () {
-  this.route("profile", function () {
-    this.route("detail", { path: "/:type/:name/detail" });
+Router.map(function() {
+  this.route('profile', function() {
+    this.route('detail', { path: '/:type/:name/detail' });
   });
-  this.route('mobile-not-support');
 });
 
 export default Router;
